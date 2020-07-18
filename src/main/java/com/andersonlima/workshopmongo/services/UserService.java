1c47b01dba1294ed45948e9d3f05790c39af4017
Littletape <andersoncardosolima0@gmail.com>
2020-07-18 13:11:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/andersonlima/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.andersonlima.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonlima.workshopmongo.domain.User;
+import com.andersonlima.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
