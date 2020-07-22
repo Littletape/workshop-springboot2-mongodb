8ebcf8cda4bfe63c01b7c3c2cd54225cfa039fd1
Littletape <andersoncardosolima0@gmail.com>
2020-07-22 09:49:18 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/andersonlima/workshopmongo/domain/Post.java
+++ src/main/java/com/andersonlima/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.andersonlima.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.andersonlima.workshopmongo.dto.AuthorDTO;
+import com.andersonlima.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String body;
 
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -94,4 +107,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 }
