8000d81da54d6b211d6b9de44c6698e82cfc61f5
Littletape <andersoncardosolima0@gmail.com>
2020-07-23 19:42:11 -0300

    Consulta simples com @Query

--- src/main/java/com/andersonlima/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/andersonlima/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,17 @@ package com.andersonlima.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.andersonlima.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//Query do mongoDB equivalente ao Select do SQL
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text); 
+	
 	List<Post> findByTitleContainingIgnoreCase(String text); //QueryMethod
 }
